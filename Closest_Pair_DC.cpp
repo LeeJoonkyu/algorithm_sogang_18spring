65b8ce0b84d9ff15b2a9dd1e21ed7b80c555d85c
Joonkyu <ljk6463@gmail.com>
2018-03-28 14:06:48 +0900

    CP start

---
+++ Closest_Pair_DC.cpp
@@ -0,0 +1,18 @@
+#include <cstdio>
+#include <algorithm>
+double Closest_Pair_DC();
+void Sort_by_X_or_Y();
+int main() {
+	unsigned int l, r;
+	unsigned int *p1, *p2;
+	double *x, *y;
+	unsigned int *tmp1, *tmp2, *tmp3;
+	unsigned int thr;
+	//입력은 test케이스 갯수(line1), 점갯수 및 thr값.
+	freopen("A.txt", "r", stdin);
+
+
+
+	return 0;
+
+}
